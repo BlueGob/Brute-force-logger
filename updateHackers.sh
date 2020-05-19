59bde2c26cfd9581c47c0eea1c6f3df2973e7fc1
Fedi Takeli <fadi249@hotmail.fr>
2020-05-19 07:57:23 +0100

    Create updateHackers.sh

---
+++ updateHackers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+grep -oE '([0-9]*\.){3}[0-9]*' /var/log/auth.log | uniq | tac > temp.txt
+
+cpt=0
+while read line; do
+line=${line##*( )}
+query="select hacker_id from hack.hackers where hacker_ip=\"${line}\""
+var=$(mysql --password="your_password" -B --disable-column-names -e "${query}" 2> /dev/null )
+if [ -z "$var" ]; then
+	query="insert into hack.hackers (hacker_ip) values (\"${line}\")"
+	mysql --password="your_password" -e "${query}" 2> /dev/null && let "cpt++"
+fi
+done < temp.txt
+
+wakt=`date +"%d-%m-%Y"`
+echo "$wakt: there has been $cpt new IP addresses added" >> /var/log/hack.log
+rm temp.txt
