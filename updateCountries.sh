4b8569cb2b30de87c18afa22732e1dafeb83b1c7
Moslem Gannoun <moslemgannoun@gmail.com>
2024-09-04 13:53:05 +0100

    Updates (#1)

--- updateCountries.sh
+++ updateCountries.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
+source config.conf
 
-sup=`mysql --password="your_password" -B --disable-column-names -e "SELECT max(hacker_id) from hackers" hack 2>> /dev/null`
-inf=`mysql --password="your_password" -B --disable-column-names -e "SELECT hacker_id from hack.hackers where country is null limit 1" 2>> /dev/null`
+sup=`mysql -u$user -p$pass -B --disable-column-names -e "SELECT max(hacker_id) from hackers" hack 2>> /dev/null`
+inf=`mysql -u$user -p$pass -B --disable-column-names -e "SELECT hacker_id from hack.hackers where country is null or country='' limit 1" 2>> /dev/null`
+
+if [[ -z "$inf" ]]; then
+	echo "No countries to update"
+	exit 0
+fi
+
 for ((i = $inf; i <= $sup; i++))
 do
 	querysel="select hacker_ip from hack.hackers where hacker_id=${i}"
-	ip=$(mysql --password="your_password" -B --disable-column-names -e "${querysel}" 2>> errlog/errlogsel )
-	echo "${ip}" | ./ipapi.py > iptemp
+	ip=$(mysql -u$user -p$pass  -B --disable-column-names -e "${querysel}" 2>> /dev/null )
+	echo "${ip}" | python3 ipapi.py > iptemp
 	country=`cut iptemp -d ';' -f 1`
 	city=`cut iptemp -d ';' -f 2`
 	timezone=`cut iptemp -d ';' -f 3`
 	queryins="update hack.hackers set country = \"${country}\", city = \"${city}\", timezone = \"${timezone}\" where hacker_id=${i}"
-	mysql --password="your_password" -e "${queryins}" 2>> errlog/errlogins
+	mysql -p$pass -e "${queryins}" 2>> /dev/null
 done
 rm iptemp
